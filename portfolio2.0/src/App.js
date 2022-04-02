a201cc361a8b864d0cf086d618bc86861c351b3e
joe <hudakjoe@msn.com>
2022-04-01 20:50:08 -0400

    header good to go

--- portfolio2.0/src/App.js
+++ portfolio2.0/src/App.js
@@ -1,29 +1,19 @@
+import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import Header from './components/Header';
 import Footer from './components/Footer';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mobile-header">
+        <Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+      </div>
+      <div>
+        <main>{renderTab()}</main>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
