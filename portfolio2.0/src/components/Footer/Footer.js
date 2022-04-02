6008edc50da866c76e4ceafa13286f96088de777
joe <hudakjoe@msn.com>
2022-04-02 17:08:22 -0400

    added a stackoverflow icon and link

--- portfolio2.0/src/components/Footer/Footer.js
+++ portfolio2.0/src/components/Footer/Footer.js
@@ -29,6 +29,19 @@ function Footer() {
           ></img>
         </a>
       </div>
+      <div>
+        <a
+          href="https://stackoverflow.com/users/18685732/mrfish24"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={require('../../assets/logos/stack-overflow-icon.png')}
+            alt="stackoverflow"
+            className="logo"
+          ></img>
+        </a>
+      </div>
     </footer>
   );
 }
