51d8792cbfecb2f70e0151a9c7b5b8475cc97bdf
joe <hudakjoe@msn.com>
2022-04-13 21:40:34 -0400

    one more last chance gh pgs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HasRouter as Router } from 'react-router-dom';
+import { HashRouter, HashRouter as Router } from 'react-router-dom';
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
